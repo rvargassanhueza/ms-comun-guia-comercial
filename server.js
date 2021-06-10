2ed4678f7fa2269055ce705c8f34894920095564
Rodrigo Vargas <rodrigovargassanhueza@gmail.com>
2021-06-10 15:30:17 -0400

    ajuste env puerto a8000

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ const cors = corsMiddleware({
 server.pre(cors.preflight);
 server.use(cors.actual);
 
-const port = process.env.API_PORT;
+const port = process.env.API_PORT || 3002;
 
 server.listen(port, () => {
   console.log({ name: "Server startup", 
